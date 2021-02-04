bba05dac1f835deec2c55268d3bebb2fe84aacd3
Mauro Rocha <mauror@mauror.com.br>
2021-02-03 21:46:47 -0300

    WIP Queimar um cupom do sistema

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :promotion
+
+  enum status: { active: 0, inactivate: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get '/', to: 'home#index'
   root 'home#index'
   resources :promotions, only: %i[index show new create] do
-    post 'generate_coupons', on: :member
+    post 'generate_coupons', on: :member    
   end  
+  resources :coupons, only: [] do
+    post 'inactivate', on: :member
+  end
 end
